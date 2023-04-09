3f426f5143c92cfcd892ed9b8a948957a867ac09
Münteha Yavuz <110225035+muntehayvz@users.noreply.github.com>
2023-04-09 14:47:38 +0300

    Word Game Added

---
+++ GameProject/Assets/GameFolder/Scripts/WordBank.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WordBank : MonoBehaviour
+{
+    private List<string> originalWords = new List<string>()
+    {
+        "private", "string" , "printf" , "Start" , "string" , "empty" , "Update", "Congrats!"
+};
+
+    private List<string> workingWords = new List<string>();
+
+    private void Awake()
+    {
+        workingWords.AddRange(originalWords);
+        Shuffle(workingWords);
+        ConverToLower(workingWords);
+
+    }
+
+    private void Shuffle(List<string> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            int random = Random.Range(i, list.Count);
+            string temporary = list[i];
+
+            list[i] = list[random];
+            list[random] = temporary;
+        }
+        
+    }
+
+    private void ConverToLower(List<string> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+            list[i] = list[i].ToLower();
+
+    }
+
+    public string GetWord()
+    {
+        string newWord = string.Empty;
+
+        if (workingWords.Count != 0)
+        {
+            newWord = workingWords.Last();
+            workingWords.Remove(newWord);
+        }
+
+        return newWord;
+    }
+}
